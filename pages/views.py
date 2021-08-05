a3bb6da4babf6e8267a8ec5ec114f18792ce53c1
mbuguap <petrmbugua@gmail.com>
2021-08-05 17:02:33 +0300

    posts

--- pages/views.py
+++ pages/views.py
@@ -8,7 +8,7 @@ from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
-    template_name = 'home.html'
+    template_name = 'index.html'
 
 
 class AboutPageView(TemplateView):
